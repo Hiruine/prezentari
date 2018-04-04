c169377d2528ba569b77fe7e3cf897739edb2b21
Radu Madiudin <rmadiudin@endava.com>
2018-04-04 17:50:46 +0300

    project exemaples

---
+++ JsonBinding/jackson/src/main/java/com/endava/data/Employee.java
@@ -0,0 +1,11 @@
+package com.endava.data;
+
+public class Employee {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String position;
+    private double salary;
+
+
+}
